e4ed276a84e3fed72d501e0a1cda4ff609836a1e
Pascal Rusch <git@paru.ch>
2020-12-20 20:14:14 +0100

    add to Homescreen button added

--- PwaDemo/js/app.js
+++ PwaDemo/js/app.js
@@ -37,3 +37,61 @@ if ("serviceWorker" in navigator) {
             .catch(err => console.log("service worker not registered", err));
     });
 }
+
+// add to homescreen btn
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    // Log install to analytics
+    console.log('INSTALL: Success');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    let displayMode = 'browser tab';
+    if (navigator.standalone) {
+        displayMode = 'standalone-ios';
+    }
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        displayMode = 'standalone';
+    }
+    // Log launch display mode to analytics
+    console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    window.matchMedia('(display-mode: standalone)').addListener((evt) => {
+        let displayMode = 'browser tab';
+        if (evt.matches) {
+            displayMode = 'standalone';
+        }
+        // Log display mode change to analytics
+        console.log('DISPLAY_MODE_CHANGED', displayMode);
+    });
+});
